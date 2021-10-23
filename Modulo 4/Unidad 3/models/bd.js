03bcf174b6f20946d12e0160db30171a94a2ce45
Eliana Gadea <eliana.gadea@gmail.com>
2021-10-23 20:32:56 -0300

    all

--- Modulo 4/Unidad 3/models/bd.js
+++ Modulo 4/Unidad 3/models/bd.js
@@ -1,24 +1,15 @@
 var mysql = require('mysql');
-var util = require('util');
-<<<<<<< HEAD
+var util  = require('util');
 
-var pool  = mysql.createPool({
-  connectionLimit : 10,
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB_NAME
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME
 });
-=======
-var pool  = mysql.createPool({
-  connectionLimit : 10,
-  host            : process.env.MYSQL_HOST,
-  user            : process.env.MYSQL_USER,
-  password        : process.env.MYSQL_PASSWORD,
-  database        : process.env.MYSQL_DB_NAME
-})
->>>>>>> 374aaacd5c78c0f6fb85b1630d5b8ed753633b24
 
 pool.query = util.promisify(pool.query);
+
 
 module.exports = pool;
